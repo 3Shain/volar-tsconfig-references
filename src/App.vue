6b4eacbe4ba80b8e81391558b3a1935e9cdbe394
3Shain <san3shain@outlook.com>
2021-10-26 18:14:50 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import {} from './ok'; // import ts source
 import {} from 'mock'; // paths defined in tsconfig.json, expect no errors
+import.meta.env; // expect an error here! because types:['node'] overwrites types:['vite/client']. See main.ts line 7
 </script>
 <template>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,4 @@ import App from './App.vue';
 createApp(App).mount('#app');
 
 import {} from 'mock';
+import.meta.env; // the error is expected! because types:['node'] overwrites types:['vite/client']
